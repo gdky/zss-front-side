5e789a1278a1b8587221248f8da8e43e7ecc6a05
manley <manley.chung@foxmail.com>
2017-09-25 00:40:33 +0800

    0901

--- src/module/home/center.jsx
+++ src/module/home/center.jsx
@@ -1,10 +1,25 @@
 import React from 'react';
 import {Button} from 'antd'
+import { Button, Row, Col, Icon } from 'antd'
 
 const c = React.createClass({
-    render(){
+    render() {
         return <div className="wrap">
             管理端主页
+            <Row gutter={16}>
+            <Col className="gutter-row" span={6}>
+              <div className="gutter-box"><Icon type="share-alt" style={{fontSize:'40px',color:'#76d0a3'}}/></div>
+            </Col>
+            <Col className="gutter-row" span={6}>
+              <div className="gutter-box">.ant-col-6</div>
+            </Col>
+            <Col className="gutter-row" span={6}>
+              <div className="gutter-box">.ant-col-6</div>
+            </Col>
+            <Col className="gutter-row" span={6}>
+              <div className="gutter-box">.ant-col-6</div>
+            </Col>
+          </Row>
         </div>
     }
 });
